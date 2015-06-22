cff35d6e1aa85177dbbdba09dfc122bdf552dd3a
AmyBeall <amylouisebeall@gmail.com>
2015-06-22 14:11:08 -0700

    fixed factory

--- public/app/controllers/jobsCtrl.js
+++ public/app/controllers/jobsCtrl.js
@@ -1,25 +1,29 @@
 angular.module('jobsCtrl', [])
-.controller('jobsController', function($scope, $sce, $filter, $q, $http){
+.controller('jobsController', function($scope, $sce, $filter, jobFactory){
 
-	var jobs = [],
+	var filtered_jobs = [],
 		count = 1;
-	$scope.filtered_jobs = [];
 	$scope.pages = [];
 
+	$scope.renderHtml = function(htmlCode){
+		return $sce.trustAsHtml(htmlCode);
+	}
 	$scope.count_down = function(){
 		if (count > 1 && count <= 12){
-			count = count - 1;
+			count -= 1;
 			paginate();
 		}
 	}
 	$scope.count_up = function(){
 		if (count >= 1 && count < 12){
-			count = count + 1;
+			count += 1;
 			paginate();
 		}
 	}
 	function paginate(){
-		if (count == 2) {
+		if (count == 1){
+			addPages(0,5);
+		} else if(count == 2) {
 			addPages(5,10);
 		} else if (count == 3) {
 			addPages(10,15);
@@ -43,61 +47,20 @@ angular.module('jobsCtrl', [])
 			addPages(55,60);
 		} 
 	}
+
 	function addPages(start, finish){
 		$scope.pages = [];
 		for(var i = start; i < finish; i++){ 
-				$scope.pages.push($scope.filtered_jobs[i]);
+				$scope.pages.push(filtered_jobs[i]);
 			}
 	}
-	$scope.renderHtml = function(htmlCode){
-		return $sce.trustAsHtml(htmlCode);
-	}
 
-	getJobs = function(){
-		var promise1 = $http({
-			url:'/api/gitJobs.json',
-			method: 'get'
-			}).success(function(data){
-
-				for(i in data){
-					data[i].company = data[i].company;
-					data[i].job_title = data[i].title;
-					data[i].location = data[i].location;
-					data[i].date = new Date(data[i].created_at);
-					data[i].git_url = data[i].url;
-					data[i].url = data[i].company_url;
-					data[i].description = data[i].description;
-					jobs.push(data[i]);
-				}
-				return data.location;
-			});	
-		var promise2 = $http({
-			url:'/api/indeedJobs.json',
-			method: 'get'
-			}).success(function(output){
-
-				var data = output.response.results[0].result;
-					for(i in data){
-						data[i].company = data[i].company[0];
-						data[i].job_title = data[i].jobtitle[0];
-						data[i].location = data[i].formattedLocationFull[0];
-						data[i].date = new Date(data[i].date[0]);
-						data[i].indeed_url = data[i].url[0];
-						data[i].description = data[i].snippet[0];
-						jobs.push(data[i]);
-					}
-				var output = data;	
-				return output;		
-			});
-		$q.all([promise1, promise2]).then(function(results){
-			$scope.filtered_jobs = $filter('orderBy')(jobs, '-date');
+	(function getJobs(){
+		jobFactory.getJobsData.then(function(response){
+			filtered_jobs = $filter('orderBy')(response, '-date');
 		}).then(function(){
-			if (count == 1){
-				addPages(0,5);
-			}	
-		});
-	}
-	
-	getJobs();	
+			addPages(0,5);
+		});;
+	})();
 
 });	
--- public/app/services/jobsService.js
+++ public/app/services/jobsService.js
@@ -1,50 +1,51 @@
 angular.module('jobsService', [])
 .factory('jobFactory', function($http, $q){
-	var factory = this;
-	jobs = [];
+	var factory = this,
+		filtered_jobs = [];
 
-	// factory.getJobs = function(){
-	// 	var deferred = $q.defer();
-	// 	var promise1 = $http({
-	// 		url:'/api/gitJobs.json',
-	// 		method: 'get'
-	// 		}).success(function(data){
-	// 			for(i in data){
-	// 				data[i].company = data[i].company;
-	// 				data[i].job_title = data[i].title;
-	// 				data[i].location = data[i].location;
-	// 				data[i].date = new Date(data[i].created_at);
-	// 				data[i].git_url = data[i].url;
-	// 				data[i].url = data[i].company_url;
-	// 				data[i].description = data[i].description;
-	// 				jobs.push(data[i]);
-	// 			}
-	// 			return data;
-	// 		});	
-	// 	var promise2 = $http({
-	// 		url:'/api/indeedJobs.json',
-	// 		method: 'get'
-	// 		}).success(function(output){
-	// 			data = output.response.results[0].result;
-	// 				for(i in data){
-	// 					data[i].company = data[i].company[0];
-	// 					data[i].job_title = data[i].jobtitle[0];
-	// 					data[i].location = data[i].formattedLocationFull[0];
-	// 					data[i].date = new Date(data[i].date[0]);
-	// 					data[i].indeed_url = data[i].url[0];
-	// 					data[i].description = data[i].snippet[0];
-	// 					jobs.push(data[i]);
-	// 				}	
-	// 		});
-	// 	$q.all([promise1, promise2]).then(function(results){
-	// 		var data = [];
-	// 	        angular.forEach(results, function(result) {
-	// 	          data = data.concat(result.data);
-	// 	          console.log(result.data);
-	// 	        });
- //        	return data;
-	// 	});
-			
-	// };
+		var gitAPI = $http({
+			url:'/api/gitJobs.json',
+			method: 'get'
+			}).then(function(response){
+				return response.data;
+			})
+		gitAPIdata = gitAPI.then(function(data){
+			for(i in data){
+				data[i].company = data[i].company;
+				data[i].job_title = data[i].title;
+				data[i].location = data[i].location;
+				data[i].date = new Date(data[i].created_at);
+				data[i].git_url = data[i].url;
+				data[i].url = data[i].company_url;
+				data[i].description = data[i].description;
+				filtered_jobs.push(data[i]);
+				}
+			return data;
+			});	
+		
+		var indeedAPI = $http({
+			url:'/api/indeedJobs.json',
+			method: 'get'
+			}).then(function(response){
+				return response.data.response.results[0].result;
+			})
+		indeedAPIdata = indeedAPI.then(function(data){
+			for(i in data){
+				data[i].company = data[i].company[0];
+				data[i].job_title = data[i].jobtitle[0];
+				data[i].location = data[i].formattedLocationFull[0];
+				data[i].date = new Date(data[i].date[0]);
+				data[i].indeed_url = data[i].url[0];
+				data[i].description = data[i].snippet[0];
+				filtered_jobs.push(data[i]);
+			}
+			return data;
+		})
+	
+		factory.getJobsData = $q.all([gitAPIdata, indeedAPIdata]).then(function(results){
+			filtered_jobs = filtered_jobs;
+			return filtered_jobs;
+		})
+		
 	return factory;	
 });	
